10c8d6041c66cce249df3e1cecdbe7faff02249c
CarlinhosHS <81194127+CarlinhosHS@users.noreply.github.com>
2026-04-09 00:16:04 -0300

    Update ai_model.py

--- ai_model.py
+++ ai_model.py
@@ -1,7 +1,10 @@
 from sklearn.ensemble import IsolationForest
 
 def aplicar_ia(df):
-    model = IsolationForest(contamination=0.05)
+    model = IsolationForest(
+        contamination=0.03,
+        random_state=42
+    )
 
     df["anomalia_ia"] = model.fit_predict(df[["x_t"]])
     df["anomalia_ia"] = df["anomalia_ia"].apply(lambda x: 1 if x == -1 else 0)
