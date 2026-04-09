b6312eddb08c336eb7a34aa2c46b3c1fdf6ade20
CarlinhosHS <81194127+CarlinhosHS@users.noreply.github.com>
2026-04-09 00:18:14 -0300

    Update data.py

--- data.py
+++ data.py
@@ -1,26 +1,15 @@
-import requests
 import pandas as pd
+import requests
 
 def get_data():
-    url = "https://services.swpc.noaa.gov/experimental/json/geoelectric/geoelectric_scatterplots.json"
+    url = "https://services.swpc.noaa.gov/json/planetary_k_index_1m.json"
+
     response = requests.get(url)
     data = response.json()
 
-    valores = []
+    df = pd.DataFrame(data)
 
-    for item in data:
-        if "value" in item:
-            valores.append(item["value"])
+    df["tempo"] = pd.to_datetime(df["time_tag"])
+    df["x_t"] = df["kp_index"]
 
-    df = pd.DataFrame({
-        "tempo": range(len(valores)),
-        "x_t": valores
-    })
-    return df
-
-    k = 1.5
-    df["tau"] = mediana + k * mad
-
-    df["anomalia_hess"] = (df["delta_H"] > df["tau"]).astype(int)
-
-    return df
+    return df.tail(200)
