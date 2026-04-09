f7a9ca6b0422215596afa9c741c2338430d8d370
CarlinhosHS <81194127+CarlinhosHS@users.noreply.github.com>
2026-04-08 23:38:16 -0300

    Update data.py

--- data.py
+++ data.py
@@ -17,21 +17,6 @@ def get_data():
         "x_t": valores
     })
     return df
-import numpy as np
-
-def aplicar_hess(df):
-    df["H"] = df["x_t"]
-
-    df["H1"] = df["H"].shift(1)
-    df["H2"] = df["H"].shift(2)
-
-    df["media"] = (df["H1"] + df["H2"]) / 2
-    df["delta_H"] = abs(df["H"] - df["media"])
-
-    window = 20
-
-    mediana = df["H"].rolling(window).median()
-    mad = df["H"].rolling(window).apply(lambda x: np.median(abs(x - np.median(x))))
 
     k = 1.5
     df["tau"] = mediana + k * mad
